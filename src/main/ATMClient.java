6dca3c7bc361d66b535511e21601b69d9d55c895
Andre <andredsouza15@gmail.com>
2024-03-22 16:10:07 -0400

    initial class setup

--- src/main/ATMClient.java
+++ src/main/ATMClient.java
@@ -1,7 +1,54 @@
 package main;
 
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
 public class ATMClient {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+
+    private static SecretKey oldSharedKey;
+    public static void main(String[] args) throws IOException {
+
+        oldSharedKey = KeyCipher.createSecretKey("thisismysecretkey24bytes");
+        String hostName = "localhost";
+        int portNumber = Integer.parseInt("23456");
+
+        try (
+                Socket clientSocket = new Socket(hostName, portNumber);
+
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+
+        ) {
+
+            Object fromBankServer, fromClient="";
+
+
+            //Sending the Client public key
+            out.writeObject(fromClient);
+
+
+            //Receiving Public key of KDC & Sending my ID
+            if ((fromBankServer = in.readObject()) != null) {
+
+            }//Sent the ID
+
+
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }// end of main
+
 }
--- src/main/BankServerThread.java
+++ src/main/BankServerThread.java
@@ -1,4 +1,63 @@
 package main;
 
-public class BankServerThread {
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+
+
+public class BankServerThread extends Thread {
+
+    private final Socket clientSocket;
+
+    private SecretKey oldSharedKey;
+
+
+
+    public BankServerThread(Socket socket) {
+        clientSocket = socket;
+        oldSharedKey = KeyCipher.createSecretKey("thisismysecretkey24bytes");
+
+    }
+
+    @Override
+    public void run() {
+
+
+        try (
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+        ) {
+
+            Object inputLine, outputLine="";
+
+
+            // Key exchange
+            if ((inputLine = in.readObject()) != null) {
+
+            }
+
+            // Receive client ID and send KDC Nonce & KDC ID
+            if ((inputLine = in.readObject()) != null) {
+
+                out.writeObject(outputLine);
+            }
+
+
+
+            clientSocket.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }// end of main
+
+
+
+
 }
